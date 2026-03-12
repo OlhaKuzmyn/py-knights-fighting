3014023c48267f95c49cfa1c8b51fed6e1fc6f33
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-03-12 23:47:04 +0200

    fixed knight_class.py

--- app/knight/knight_class.py
+++ app/knight/knight_class.py
@@ -14,10 +14,13 @@ class Knight:
         self.name = name
         self.power = power
         self.hp = hp
-        self.protection = 0
+        # self.protection = 0
         self.armour = armour
         self.weapon = weapon
         self.potion = potion
+        self.battle_hp = hp
+        self.battle_power = power
+        self.battle_protection = 0
 
     @classmethod
     def new_knight(cls, knight_dict: dict) -> Knight:
@@ -27,26 +30,32 @@ class Knight:
 
     def apply_armour(self) -> None:
         for part in self.armour:
-            self.protection += part["protection"]
+            self.battle_protection += part["protection"]
 
     def apply_weapon(self) -> None:
-        self.power += self.weapon["power"]
+        self.battle_power += self.weapon["power"]
 
     def apply_potion(self) -> None:
         if self.potion is not None:
             effect = self.potion.get("effect")
-            self.hp += effect.get("hp", 0)
-            self.power += effect.get("power", 0)
-            self.protection += effect.get("protection", 0)
+            self.battle_hp += effect.get("hp", 0)
+            self.battle_power += effect.get("power", 0)
+            self.battle_protection += effect.get("protection", 0)
+
+    def before_battle(self) -> None:
+        self.battle_hp = self.hp
+        self.battle_power = self.power
+        self.battle_protection = 0
 
     def prepare_for_battle(self) -> KnightForBattle:
+        self.before_battle()
         self.apply_armour()
         self.apply_weapon()
         self.apply_potion()
 
         return KnightForBattle(
             self.name,
-            self.hp,
-            self.power,
-            self.protection,
+            self.battle_hp,
+            self.battle_power,
+            self.battle_protection
         )
